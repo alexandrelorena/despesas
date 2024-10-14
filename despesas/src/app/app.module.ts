c3b6f424f9c10beac83e6af44afd88cfbc4408ad
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-10-14 09:43:58 -0300

    Início do repositório remoto

---
+++ despesas/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MonthComponent } from './components/month/month.component';
+import { AddDespesaComponent } from './components/add-despesa/add-despesa.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    MonthComponent,
+    AddDespesaComponent,
+    ContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ despesas/src/app/components/add-despesa/add-despesa.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-despesa',
+  templateUrl: './add-despesa.component.html',
+  styleUrls: ['./add-despesa.component.css'],
+})
+export class AddDespesaComponent {}
