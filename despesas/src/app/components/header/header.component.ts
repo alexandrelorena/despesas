6ac6504eaa03d32b44c80bef1a4801af402f742f
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-10-16 22:38:29 -0300

    Atualização menú

--- despesas/src/app/components/header/header.component.ts
+++ despesas/src/app/components/header/header.component.ts
@@ -10,6 +10,19 @@ export class HeaderComponent {
 
   constructor(private renderer: Renderer2) {}
 
+  ngOnInit(): void {
+    // Verifica se o tema escuro está armazenado no localStorage
+    const isDarkTheme = localStorage.getItem('isDarkTheme') === 'true';
+    this.isDarkMode = isDarkTheme; // Atualiza o estado da variável
+
+    // Aplica a classe correspondente ao tema ao carregar
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-theme');
+    } else {
+      this.renderer.addClass(document.body, 'light-theme');
+    }
+  }
+
   toggleMode(): void {
     this.isDarkMode = !this.isDarkMode;
     if (this.isDarkMode) {
--- despesas/src/app/components/menu/menu.component.ts
+++ despesas/src/app/components/menu/menu.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {}
+export class MenuComponent {
+  months: string[] = [
+    'jan',
+    'fev',
+    'mar',
+    'abr',
+    'mai',
+    'jun',
+    'jul',
+    'ago',
+    'set',
+    'out',
+    'nov',
+    'dez',
+  ];
+
+  // months: string[] = [
+  //   'Janeiro',
+  //   'Fevereiro',
+  //   'Março',
+  //   'Abril',
+  //   'Maio',
+  //   'Junho',
+  //   'Julho',
+  //   'Agosto',
+  //   'Setembro',
+  //   'Outubro',
+  //   'Novembro',
+  //   'Dezembro',
+  // ];
+
+  @Output() monthSelected = new EventEmitter<string>();
+
+  selectMonth(month: string) {
+    this.monthSelected.emit(month);
+  }
+}
